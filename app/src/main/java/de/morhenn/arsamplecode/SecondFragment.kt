322a7c8a42d8e61a6e877dc55d6662702984484b
morhenny <yannik.morhenn@web.de>
2022-02-24 23:52:46 +0100

    migrate to local sceneview clone

--- app/src/main/java/de/morhenn/arsamplecode/SecondFragment.kt
+++ app/src/main/java/de/morhenn/arsamplecode/SecondFragment.kt
@@ -1,18 +1,26 @@
 package de.morhenn.arsamplecode
 
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
 import com.google.ar.core.Config
+import com.google.ar.sceneform.rendering.ModelRenderable
+import com.google.ar.sceneform.rendering.Renderable
 import de.morhenn.arsamplecode.databinding.FragmentSecondBinding
 import io.github.sceneview.ar.ArSceneView
+import io.github.sceneview.ar.node.ArNode
+import io.github.sceneview.ar.scene.PlaneRenderer
+import io.github.sceneview.model.await
 
 class SecondFragment : Fragment() {
 
     private var _binding: FragmentSecondBinding? = null
     private val binding get() = _binding!!
+    private var cubeModel: Renderable? = null
 
     lateinit var sceneView: ArSceneView
 
@@ -24,10 +32,50 @@ class SecondFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        lifecycleScope.launchWhenCreated {
+            cubeModel = ModelRenderable.builder()
+                .setSource(context, Uri.parse("models/cube.glb"))
+                .setIsFilamentGltf(true)
+                .await()
+        }
         sceneView = binding.sceneView
         sceneView.configureSession { config ->
             config.cloudAnchorMode = Config.CloudAnchorMode.ENABLED
+            config.planeFindingMode = Config.PlaneFindingMode.HORIZONTAL_AND_VERTICAL
             config.updateMode = Config.UpdateMode.LATEST_CAMERA_IMAGE
         }
+        sceneView.planeRenderer.planeRendererMode = PlaneRenderer.PlaneRendererMode.RENDER_ALL
+
+        sceneView.onTouchAr = { hitResult, _ ->
+            val node = ArNode()
+            node.anchor = hitResult.createAnchor()
+            node.setRenderable(cubeModel)
+            sceneView.addChild(node)
+        }
+    }
+
+    override fun onPause() {
+        sceneView.onPause(this)
+        super.onPause()
+    }
+
+    override fun onResume() {
+        sceneView.onResume(this)
+        super.onResume()
+    }
+
+    override fun onStop() {
+        sceneView.onStop(this)
+        super.onStop()
+    }
+
+    override fun onStart() {
+        sceneView.onStart(this)
+        super.onStart()
+    }
+
+    override fun onDestroy() {
+        sceneView.onDestroy(this)
+        super.onDestroy()
     }
 }
